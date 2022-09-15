903f9b996cd2809c9841cba68da22081d2f10b48
unknown <efkafi@gmail.com>
2022-09-15 23:05:23 +0200

    Initial commit

---
+++ src/main/java/net/casper/breadtech/BreadTech.java
@@ -0,0 +1,18 @@
+package net.casper.breadtech;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.fml.common.Mod;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod("breadtech")
+public class BreadTech
+{
+
+
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public BreadTech() {
+
+    }
+}
